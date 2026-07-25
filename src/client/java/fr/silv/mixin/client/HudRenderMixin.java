e661e6e4f93e1f597c0b6710167dc6d5b8a981bb
Wang John <john-wang@hotmail.fr>
2026-07-25 19:57:04 +0200

    Fix HudRenderMixin crash on Minecraft 26.2

--- src/client/java/fr/silv/mixin/client/HudRenderMixin.java
+++ src/client/java/fr/silv/mixin/client/HudRenderMixin.java
@@ -5,14 +5,14 @@ import fr.silv.items.MuseumHighlightHandler;
 import fr.silv.hud.widget.HudWidgetManager;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.DeltaTracker;
-import net.minecraft.client.gui.Gui;
+import net.minecraft.client.gui.Hud;
 import net.minecraft.client.gui.GuiGraphicsExtractor;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(Gui.class)
+@Mixin(Hud.class)
 public class HudRenderMixin {
 
     @Inject(method = "extractRenderState", at = @At("TAIL"))
